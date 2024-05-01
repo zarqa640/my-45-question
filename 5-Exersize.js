16d3304a49818f5256d12c5baa7bac8a99b8b7cd
zarqa <160513411+zarqa640@users.noreply.github.com>
2024-05-02 01:06:58 +0500

    Add files via upload

---
+++ 5-Exersize.js
@@ -0,0 +1,3 @@
+var personName = "Albert Einstein";
+var massage = "once said,\" A person who never made a mistake never try anything new\"";
+console.log(personName, massage);
---
+++ 5-Exersize.ts
@@ -0,0 +1,4 @@
+let personName="Albert Einstein";
+
+let massage="once said,\" A person who never made a mistake never try anything new\"";
+console.log(personName,massage);
