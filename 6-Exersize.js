9e03e4b2086fdb2ff4e6598a49772d83dafb80f6
zarqa <160513411+zarqa640@users.noreply.github.com>
2024-05-02 01:25:37 +0500

    Add files via upload

---
+++ 6-Exersize.js
@@ -0,0 +1,4 @@
+var whiteSpace = "\n\tzarka khan\t\n";
+console.log(whiteSpace);
+var withoutWhiteSpace = "zarka khan";
+console.log(withoutWhiteSpace);
---
+++ 6-Exersize.ts
@@ -0,0 +1,4 @@
+let whiteSpace="\n\tzarka khan\t\n"
+console.log(whiteSpace);
+let withoutWhiteSpace="zarka khan"
+console.log(withoutWhiteSpace);
