34e6af831bb47c643eb342f2654de3961856823b
zarqa <160513411+zarqa640@users.noreply.github.com>
2024-05-01 23:34:02 +0500

    Add files via upload

---
+++ 2-personal-massage.js
@@ -0,0 +1,3 @@
+"use strict";
+let personName = ("zarka khan");
+console.log("hellow", personName, "would you like to learn some python today?");
---
+++ 2-personal-massage.ts
@@ -0,0 +1,2 @@
+let personName =("zarka khan");
+console.log("hellow",personName,"would you like to learn some python today?");
